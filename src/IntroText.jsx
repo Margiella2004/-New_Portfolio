6f7ac375bad07ea60dbde4d26e88784ad9cba924
Jonathan Ramesh <jonathanrameshdesign@gmail.com>
2025-12-13 10:27:31 -0800

    added tags to it

--- src/IntroText.jsx
+++ src/IntroText.jsx
@@ -2,6 +2,7 @@ import './IntroText.css'
 
 const lineImage = "https://www.figma.com/api/mcp/asset/3aabee4c-2f03-4b7b-9acb-67ee915e883d"
 const arrowImage = "https://www.figma.com/api/mcp/asset/ede21216-651c-4981-b9b1-42facbc7fea5"
+const linkArrowImage = "https://www.figma.com/api/mcp/asset/9022f730-05eb-470d-afb4-a302a6d5991a"
 
 export default function IntroText({ paddingX = 72 }) {
   return (
@@ -27,10 +28,36 @@ export default function IntroText({ paddingX = 72 }) {
           </div>
         </div>
 
-        {/* Right: Intro text */}
-        <p className="intro-text">
-          Hi, I'm a UX & 3D Designer who combines coding and design values in order to create better products
-        </p>
+        {/* Right: Intro text and links */}
+        <div className="intro-text-container">
+          <p className="intro-text">
+            Hi, I'm a UX & 3D Designer who combines coding and design values in order to create better products
+          </p>
+
+          {/* Link Buttons */}
+          <div className="link-buttons">
+            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" className="link-button">
+              <span className="link-button-text">linkedin</span>
+              <div className="link-button-arrow">
+                <img src={linkArrowImage} alt="" className="link-arrow-image" />
+              </div>
+            </a>
+
+            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" className="link-button">
+              <span className="link-button-text">resume</span>
+              <div className="link-button-arrow">
+                <img src={linkArrowImage} alt="" className="link-arrow-image" />
+              </div>
+            </a>
+
+            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" className="link-button">
+              <span className="link-button-text">github</span>
+              <div className="link-button-arrow">
+                <img src={linkArrowImage} alt="" className="link-arrow-image" />
+              </div>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   )
